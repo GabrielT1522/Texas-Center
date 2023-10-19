a75d011bea14b4d2e8a31ff02c4bdff0aa3255cd
Gabriel Torres <GabrielT1522@gmail.com>
2023-10-19 11:18:04 -0500

    implement make table in statehs

--- TXC-ITDN/js/statehs.js
+++ TXC-ITDN/js/statehs.js
@@ -106,7 +106,22 @@ function buildArrayData(API_DATA, tradeType, headerCounter) {
   return API_DATA;
 }
 
+function makeTableHTML(myArray) {
+  var result = '<table id="myTable" border=1>';
+  for(var i=0; i<myArray.length; i++) {
+      
+          result += '<tr class="header">';
+          for(var j=0; j<myArray[i].length; j++){
+              result += "<td>"+myArray[i][j]+"</td>";
+          }
+          result += "</tr>";
+      
+  }
+  result += "</table>";
 
+  stopTimer();
+  return result;
+}
 
 
 // Convert to csv file seperated by '^'
